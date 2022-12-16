082586226a4d1e1e54ba29d481cebf8d9df4d667
Juravl <alexey.jurov@gmail.com>
2022-12-16 17:32:16 +0700

    Добавьте файлы проекта.

---
+++ TernOp.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int a;
+int b;
+int c;
+
+int main()
+{
+    int temp;
+
+    printf("Swap the values of the variables a, b and c so that a ≥ b ≥ c.\n");
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    a = (a < b&& a < c&& c < b) ? temp = a, b : a;
+}
